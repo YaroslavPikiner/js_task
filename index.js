9e11587d461cbd8ee49edee39d82dfe08033b7b8
Yaroslav <yaroslav.pikiner@chiws.com>
2021-04-20 14:18:54 +0300

    add pop  method

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function MyArray() {
-  this.myPush = function(value) {
+  this.myPush = function (value) {
     return (this[this.length++] = value);
   };
-
+  this.myPop = function (value) {
+    return this.splice(this.length - 1, 1);
+  };
 }
 
 MyArray.prototype = Array.prototype;
 const collection = new MyArray();
 
 collection.myPush(56); // this is not working.
-collection.myPush(2);
+collection.myPush("2");
+collection.myPush(true);
+collection.myPush(NaN);
+collection.myPop();
+collection.myPop();
+collection.myPop();
+
 console.log(collection); // Array { '0': 56, '1': 2, myPush: [Function (anonymous)], length: 2 }
