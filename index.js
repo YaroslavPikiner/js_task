a2d32022e82e7955f5e4db02607c19ab5363159d
Yaroslav <yaroslav.pikiner@chiws.com>
2021-04-21 17:44:34 +0300

    add itemator task

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function MyArray() {
   for (let i = 0; i < arguments.length; i++) {
-    if(arguments[i] === undefined) {
-      console.log('попався')
+    if (arguments[i] === undefined) {
+      console.log("попався");
     }
     this[i] = arguments[i];
   }
@@ -11,6 +11,31 @@ function MyArray() {
     },
   });
 }
+
+MyArray.prototype[Symbol.iterator] = function () {
+  let current = 0;
+  let last = this.length - 1;
+  console.log(this, "this");
+  return {
+    next() {
+
+      if (current === undefined) {
+        current = last;
+      }
+      if (current <= last) {
+        return {
+          done: false,
+          value: current++,
+        };
+      } else {
+        return {
+          done: true,
+          value: undefined,
+        };
+      }
+    },
+  };
+};
 
 MyArray.prototype.pushsing = function () {
   if (arguments) {
@@ -51,56 +76,36 @@ MyArray.prototype.pop = function () {
   return val;
 };
 
-// MyArray.prototype.reduce = function (callback, result) {
-//   let i = 0;
-//   if (arguments.length < 2) {
-//     i = 1;
-//     result = this[0];
-//   }
-//   for (; i < this.length; i++) {
-//     result = callback(result, this[i], i, this);
-//   }
-//   return result;
-// };
-
-
-MyArray.prototype.reduce = function(callback, initVal) {
+MyArray.prototype.reduce = function (callback, initVal) {
   let acc = initVal;
- let startAtIndex = 0;
+  let startAtIndex = 0;
 
- if (initVal === undefined) {
-   acc = this[0];
-   startAtIndex = 1;
- }
-
- for (let index = startAtIndex; index < this.length; index += 1) {
-   acc = callback(acc, this[index], index, this);
- }
-
- return acc;
-}
-
-
-MyArray.prototype.sortedFunc = function(first, second) {
-  if(first > second) {
-    return 1
+  if (initVal === undefined) {
+    acc = this[0];
+    startAtIndex = 1;
   }
 
-  if(second < first) {
-    return -1
+  for (let index = startAtIndex; index < this.length; index += 1) {
+    acc = callback(acc, this[index], index, this);
   }
 
-  return 0
-  
-} 
+  return acc;
+};
+
+MyArray.prototype.sortedFunc = function (first, second) {
+  if (first > second) {
+    return 1;
+  }
+  if (second < first) {
+    return -1;
+  }
+  return 0;
+};
 // test sort
 MyArray.prototype.sort = function (callback = this.sortedFunc) {
-
   for (let i = 1; i < this.length; i++) {
-
     for (let j = i; j > 0; j--) {
-
-      if (callback(this[j] , this[j - 1])) {
+      if (callback(this[j], this[j - 1])) {
         const temp = this[j];
         this[j] = this[j - 1];
         this[j - 1] = temp;
@@ -113,22 +118,26 @@ MyArray.prototype.sort = function (callback = this.sortedFunc) {
 };
 
 
-MyArray.prototype.fromTo = function() {}
-MyArray.prototype.toString = function() {
-    let result = ''
-    for(let i = 0; i < this.length; i++) {
-      result += `${this[i]},`
-    }
-    return result.substring(0, result.length - 1)
-}
 
-let array = new MyArray(2,3,4);
+MyArray.prototype.toString = function () {
+  let result = "";
+  for (let i = 0; i < this.length; i++) {
+    result += `${this[i]},`;
+  }
+  return result.substring(0, result.length - 1);
+};
 
+let array = new MyArray(1, 2, 3, 4, 5,1,3,123,2,53,65,678,689);
+
+let arr = [...array];
+console.log(arr);
+
+// array.map((item,i) => {
+//   console.log(item + '-' + i)
+// })
 
 // console.log(array.sort((a,b) => a < b ));
-// console.log(array.reduce((prev, curr) => prev + curr));
-console.log(array.toString());
+// console.log(array.from());
 
-// 1 отловить empty string
-// 2 создать tostring
 // 3 создать from to
+MyArray.prototype.from = function () {};
